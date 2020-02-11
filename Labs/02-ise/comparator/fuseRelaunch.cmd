6bd8e364b6e264424ca00cc4b2680a0d4e2f5b5c
tomas.fryza <tomas.fryza@gmail.com>
2020-02-11 13:57:41 +0100

    [LAB] Adding 02-ise lab

---
+++ Labs/02-ise/comparator/fuseRelaunch.cmd
@@ -0,0 +1,1 @@
+-intstyle "ise" -incremental -o "/home/lab661/Documents/fryza/Digital-electronics-1/Labs/02-ise/comparator/top_tb00_isim_beh.exe" -prj "/home/lab661/Documents/fryza/Digital-electronics-1/Labs/02-ise/comparator/top_tb00_beh.prj" "work.top_tb00" 
