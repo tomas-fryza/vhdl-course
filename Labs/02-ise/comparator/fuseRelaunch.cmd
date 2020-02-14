0c42dc03795122f53fd220f2981ab289b30ef9c2
Tomas Fryza <tomas.fryza@gmail.com>
2020-02-14 20:29:38 +0100

    [LAB] Update 02-ise project files

--- Labs/02-ise/comparator/fuseRelaunch.cmd
+++ Labs/02-ise/comparator/fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -o "/home/lab661/Documents/fryza/Digital-electronics-1/Labs/02-ise/comparator/top_tb00_isim_beh.exe" -prj "/home/lab661/Documents/fryza/Digital-electronics-1/Labs/02-ise/comparator/top_tb00_beh.prj" "work.top_tb00" 
+-intstyle "ise" -incremental -o "/home/fryza/GIT/Digital-electronics-1/Labs/02-ise/comparator/top_tb00_isim_beh.exe" -prj "/home/fryza/GIT/Digital-electronics-1/Labs/02-ise/comparator/top_tb00_beh.prj" "work.top_tb00" 
