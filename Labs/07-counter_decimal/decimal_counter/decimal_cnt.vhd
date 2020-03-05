dda91d19fa575638887e71be26b471a97ffab1d6
Tomas Fryza <tomas.fryza@gmail.com>
2020-03-05 09:18:36 +0100

    [LAB] Renamed 06-counter_bcd to 07-counter_decimal

--- Labs/07-counter_decimal/decimal_counter/decimal_cnt.vhd
+++ Labs/07-counter_decimal/decimal_counter/decimal_cnt.vhd
@@ -1,9 +1,9 @@
 ------------------------------------------------------------------------
 --
--- Up/down BCD counter with load and carry.
+-- Up/down decimal counter with load.
 -- Xilinx XC2C256-TQ144 CPLD, ISE Design Suite 14.7
 --
--- Copyright (c) 2019-2020 Tomas Fryza
+-- Copyright (c) 2020 Tomas Fryza
 -- Dept. of Radio Electronics, Brno University of Technology, Czechia
 -- This work is licensed under the terms of the MIT license.
 --
@@ -14,42 +14,42 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;    -- Provides unsigned numerical computation
 
 ------------------------------------------------------------------------
--- Entity declaration for BCD counter
+-- Entity declaration for decimal counter
 ------------------------------------------------------------------------
-entity bcd_cnt is
+entity decimal_cnt is
 port (
     clk_i    : in  std_logic;
     srst_n_i : in  std_logic;   -- Synchronous reset (active low)
     en_i     : in  std_logic;   -- Enable
-    up_i     : in  std_logic;   -- Up direction
-    -- Load, Carry
-    cnt_o    : out std_logic_vector(4-1 downto 0)
+    up_i     : in  std_logic    -- Up direction
+    -- Load
+    -- Data outputs
 );
-end entity bcd_cnt;
+end entity decimal_cnt;
 
 ------------------------------------------------------------------------
--- Architecture declaration for BCD counter
+-- Architecture declaration for decimal counter
 ------------------------------------------------------------------------
-architecture Behavioral of bcd_cnt is
-    signal s_cnt : std_logic_vector(4-1 downto 0);
+architecture Behavioral of decimal_cnt is
+    -- TBD
 begin
 
     --------------------------------------------------------------------
-    -- p_bcd_cnt:
+    -- p_decimal_cnt:
     -- Sequential process with synchronous reset and clock enable,
-    -- which implements a two-way BCD counter.
+    -- which implements a two-way decimal counter.
     --------------------------------------------------------------------
-    p_bcd_cnt : process(clk_i)
+    p_decimal_cnt : process(clk_i)
     begin
         if rising_edge(clk_i) then  -- Rising clock edge
             if srst_n_i = '0' then  -- Synchronous reset (active low)
-                s_cnt <= (others => '0');   -- Clear all bits
+                -- WRITE YOUR CODE HERE
             elsif en_i = '1' then
                 -- WRITE YOUR CODE HERE
             end if;
         end if;
     end process p_bcd_cnt;
 
-    cnt_o <= s_cnt;
+    -- WRITE YOUR CODE HERE
 
 end architecture Behavioral;
