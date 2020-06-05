7be3dda108b6afa1ce9dd573df7011935a0c4b17
Tomas Fryza <tomas.fryza@gmail.com>
2020-06-05 21:07:27 +0200

    [NOTES] Minor modifications

--- Labs/readme2pdf.sh
+++ Labs/readme2pdf.sh
@@ -7,24 +7,24 @@ pandocparams="-V colorlinks -V urlcolor=Magenta \
 
 cd ..
 filename="labs"
-echo -n "Converting `pwd`/README.md -> ${filename}.pdf ..."
+echo -n "Converting `pwd`/README.md -> ${filename}.pdf... "
 pandoc -H Labs/head.tex README.md -o ${filename}.pdf ${pandocparams}
-echo "done"
+echo "done."
 
 filename="01-gates"
 cd Labs/${filename}
-echo -n "Converting `pwd`/README.md -> ${filename}.pdf ..."
+echo -n "Converting `pwd`/README.md -> ${filename}.pdf... "
 pandoc -H ../head.tex README.md -o ../${filename}.pdf ${pandocparams}
-echo "done"
+echo "done."
 
 filename="02-ise"
 cd ../${filename}
-echo -n "Converting `pwd`/README.md -> ${filename}.pdf ..."
+echo -n "Converting `pwd`/README.md -> ${filename}.pdf... "
 pandoc -H ../head.tex README.md -o ../${filename}.pdf ${pandocparams}
-echo "done"
+echo "done."
 
 filename="03-segment"
 cd ../${filename}
-echo -n "Converting `pwd`/README.md -> ${filename}.pdf ..."
+echo -n "Converting `pwd`/README.md -> ${filename}.pdf... "
 pandoc -H ../head.tex README.md -o ../${filename}.pdf ${pandocparams}
-echo "done"
+echo "done."
