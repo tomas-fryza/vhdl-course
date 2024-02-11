c92157aaa4a340889e6e970f31ab7975bd9fa24b
tomas-fryza <tomas.fryza@gmail.com>
2024-02-11 15:52:02 +0100

    hex_7seg renamed to bin2seg

--- solutions/lab3-segment/bin2seg.vhd
+++ solutions/lab3-segment/bin2seg.vhd
@@ -1,124 +1,134 @@
-----------------------------------------------------------
---
---! @title One-digit 7-segment display decoder
---! @author Tomas Fryza
---! Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
+-------------------------------------------------
+--! @brief One-digit 7-segment display decoder
+--! @version 1.3
+--! @copyright (c) 2018 Tomas Fryza, MIT license
 --!
---! @copyright (c) 2018 Tomas Fryza
---! This work is licensed under the terms of the MIT license
---!
+
+
+
 --! Decoder for one-digit Seven-segment display, Common Anode
 --! (active-low). Decoder defines 16 hexadecimal symbols: 0, 1,
 --! ..., 9, A, b, C, d, E, F. All segments are turn off when
 --! "blank" is high. Decimal Point is not implemented.
---
--- Hardware: Nexys A7-50T, xc7a50ticsg324-1L
--- Software: TerosHDL, Vivado 2020.2, EDA Playground
---
-----------------------------------------------------------
+--!
+--! Developed using TerosHDL, Vivado 2020.2, and
+--! EDA Playground. Tested on the Nexys A7-50T
+--! board featuring the xc7a50ticsg324-1L FPGA.
+-------------------------------------------------
 
 library ieee;
   use ieee.std_logic_1164.all;
 
-----------------------------------------------------------
--- Entity declaration for seven-segment display decoder
-----------------------------------------------------------
+-------------------------------------------------
 
-entity hex_7seg is
+entity bin2seg is
   port (
-    blank : in    std_logic;                    --! Display is clear if blank = 1
-    hex   : in    std_logic_vector(3 downto 0); --! Binary representation of one hexadecimal symbol
+    clear : in    std_logic;                    --! Clear the display
+    bin   : in    std_logic_vector(3 downto 0); --! Binary representation of one hexadecimal symbol
     seg   : out   std_logic_vector(6 downto 0)  --! Seven active-low segments in the order: a, b, ..., g
   );
-end entity hex_7seg;
+end entity bin2seg;
 
-----------------------------------------------------------
--- Architecture body for seven-segment display decoder
-----------------------------------------------------------
+-------------------------------------------------
 
-architecture behavioral of hex_7seg is
+architecture behavioral of bin2seg is
 
 begin
 
-  --------------------------------------------------------
-  -- p_7seg_decoder:
-  -- A combinational process for 7-segment display (Common
-  -- Anode) decoder. Any time "hex" or "blank" is changed,
-  -- the process is "executed". Output pin seg(6) controls
-  -- segment A, seg(5) segment B, etc.
-  --     segment A
-  --      | segment B
-  --      |  | segment C
-  --      +-+|  |   ...   segment G
-  --        ||+-+          |
-  --        |||            |
-  -- seg = "0000001"-------+
-  --
-  -- Display is clear/blank if signal blank = 1.
-  --------------------------------------------------------
+  -----------------------------------------------
+  --! p_7seg_decoder:
+  --! A combinational process for 7-segment display
+  --! (Common Anode) decoder. Any time `bin` or
+  --! `clear` is changed, the process is "executed".
+  --!
+  --!     segment A
+  --!      | segment B
+  --!      |  | segment C
+  --!      +-+|  |   ...   segment G
+  --!        ||+-+          |
+  --!        |||            |
+  --! seg = "0000001"-------+
+  --!
+  --! Display is clear/blank if input `clear` = 1.
+  -----------------------------------------------
 
-  p_7seg_decoder : process (blank, hex) is
-
+  p_7seg_decoder : process (bin, clear) is
   begin
 
-    if (blank = '1') then
-      seg <= "1111111";     -- Blanking display
+    if (clear = '1') then
+      seg <= "1111111";     -- Clear the display
     else
 
-      case hex is
+      case bin is
 
         when "0000" =>
+
           seg <= "0000001"; -- 0
 
         when "0001" =>
+
           seg <= "1001111"; -- 1
 
         -- WRITE YOUR CODE HERE
         -- 2, 3, 4, 5, 6, 7
 
         when "0010" =>
+
           seg <= "0010010"; -- 2
 
         when "0011" =>
+
           seg <= "0000110"; -- 3
 
         when "0100" =>
+
           seg <= "1001100"; -- 4
 
         when "0101" =>
+
           seg <= "0100100"; -- 5
 
         when "0110" =>
+
           seg <= "0100000"; -- 6
 
         when "0111" =>
+
           seg <= "0001111"; -- 7
 
         when "1000" =>
+
           seg <= "0000000"; -- 8
 
         -- WRITE YOUR CODE HERE
         -- 9, A, b, C, d
 
         when "1001" =>
+
           seg <= "0000100"; -- 9
 
         when "1010" =>
+
           seg <= "0001000"; -- A (10)
 
         when "1011" =>
+
           seg <= "1100000"; -- b (11)
 
         when "1100" =>
+
           seg <= "0110001"; -- C (12)
 
         when "1101" =>
+
           seg <= "1000010"; -- d (13)
 
         when "1110" =>
+
           seg <= "0110000"; -- E
 
         when others =>
+
           seg <= "0111000"; -- F
 
       end case;
--- solutions/lab3-segment/tb_bin2seg.vhd
+++ solutions/lab3-segment/tb_bin2seg.vhd
@@ -1,72 +1,64 @@
-----------------------------------------------------------
---
--- Testbench for 7-segment display decoder.
--- Nexys A7-50T, xc7a50ticsg324-1L
--- TerosHDL, Vivado v2020.2, EDA Playground
---
--- Copyright (c) 2020 Tomas Fryza
--- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
--- This work is licensed under the terms of the MIT license.
---
-----------------------------------------------------------
-
 library ieee;
   use ieee.std_logic_1164.all;
   use ieee.numeric_std.all; -- Definition of "to_unsigned"
 
-----------------------------------------------------------
--- Entity declaration for testbench
-----------------------------------------------------------
+-------------------------------------------------
 
-entity tb_hex_7seg is
-  -- Entity of testbench is always empty
-end entity tb_hex_7seg;
+entity tb_bin2seg is
+-- Entity of testbench is always empty
+end entity tb_bin2seg;
 
-----------------------------------------------------------
--- Architecture body for testbench
-----------------------------------------------------------
+-------------------------------------------------
 
-architecture testbench of tb_hex_7seg is
+architecture testbench of tb_bin2seg is
+
+  component bin2seg is
+    port (
+      clear : in    std_logic;
+      bin   : in    std_logic_vector(3 downto 0);
+      seg   : out   std_logic_vector(6 downto 0)
+    );
+  end component;
 
   -- Testbench local signals
-  signal sig_blank : std_logic;
-  signal sig_hex   : std_logic_vector(3 downto 0);
+  signal sig_clear : std_logic;
+  signal sig_bin   : std_logic_vector(3 downto 0);
   signal sig_seg   : std_logic_vector(6 downto 0);
 
 begin
 
-  -- Connecting testbench signals with decoder entity
-  -- (Unit Under Test)
-  uut_hex_7seg : entity work.hex_7seg
+  dut : component bin2seg
     port map (
-      blank => sig_blank,
-      hex   => sig_hex,
+      clear => sig_clear,
+      bin   => sig_bin,
       seg   => sig_seg
     );
 
-  --------------------------------------------------------
+  -----------------------------------------------
   -- Data generation process
-  --------------------------------------------------------
+  -----------------------------------------------
   p_stimulus : process is
   begin
 
     report "Stimulus process started";
 
-    sig_blank <= '0';    -- Normal operation
-    sig_hex   <= "0011"; -- Some default value
+    -- Normal operation
+    sig_clear <= '0';
+    sig_bin   <= "0011";
     wait for 50 ns;
 
-    sig_blank <= '1';    -- Blank display
+    -- Blank display
+    sig_clear <= '1';
     wait for 115 ns;
-    sig_blank <= '0';    -- Normal operation
+    sig_clear <= '0';
     wait for 25 ns;
 
     -- Loop for all hex values
     for ii in 0 to 15 loop
 
       -- Convert ii decimal value to 4-bit wide binary
-      -- sig_hex <= std_logic_vector(to_unsigned(ii, sig_hex'length));
-      sig_hex <= std_logic_vector(to_unsigned(ii, 4));
+      -- sig_bin <= std_logic_vector(to_unsigned(ii, sig_bin'length));
+      sig_bin <= std_logic_vector(to_unsigned(ii, 4));
       wait for 50 ns;
 
     end loop;
